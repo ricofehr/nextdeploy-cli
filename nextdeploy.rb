951a8c5a3bf66d3fe4b2eccc5d9d6ed55569f090
ricofehr <ricofehr@nextdeploy.io>
2016-05-15 15:42:01 +0200

    adapt to api v1.3

--- nextdeploy.rb
+++ nextdeploy.rb
@@ -58,10 +58,10 @@ class NextDeploy < Thor
     end
 
     # prepare post request
-    launch_req = { vm: { project_id: @project[:id], vmsize_id: @project[:vmsizes][0], user_id: @user[:id], systemimage_id: @project[:systemimages][0], technos: @project[:technos], is_auth: true, layout: @user[:layout], htlogin: @project[:login], htpassword: @project[:password], commit_id: commitid } }
+    launch_req = { vm: { project_id: @project[:id], vmsize_id: @project[:vmsizes][0], user_id: @user[:id], systemimage_id: @project[:systemimages][0], technos: @project[:technos], is_auth: true, is_prod: false, is_cached: false, is_ht: @project[:is_ht], layout: @user[:layout], htlogin: @project[:login], htpassword: @project[:password], commit_id: commitid } }
 
     response = @conn.post do |req|
-      req.url "/api/v1/vms"
+      req.url "/api/v1/vms/short"
       req.headers = rest_headers
       req.body = launch_req.to_json
     end
@@ -126,10 +126,10 @@ class NextDeploy < Thor
     error("Branch #{branch} not found") if !@branche
     
     # prepare post request
-    launch_req = { vm: { project_id: @project[:id], vmsize_id: @project[:vmsizes][0], user_id: @user[:id], technos: @project[:technos], systemimage_id: @project[:systemimages][0], is_auth: true, layout: @user[:layout], htlogin: @project[:login], htpassword: @project[:password], commit_id: "#{@branche[:commits][0]}" } }
+    launch_req = { vm: { project_id: @project[:id], vmsize_id: @project[:vmsizes][0], user_id: @user[:id], technos: @project[:technos], systemimage_id: @project[:systemimages][0], is_auth: true, is_prod: false, is_cached: false, is_ht: @project[:is_ht], layout: @user[:layout], htlogin: @project[:login], htpassword: @project[:password], commit_id: "#{@branche[:commits][0]}" } }
 
     response = @conn.post do |req|
-      req.url "/api/v1/vms"
+      req.url "/api/v1/vms/short"
       req.headers = rest_headers
       req.body = launch_req.to_json
     end
