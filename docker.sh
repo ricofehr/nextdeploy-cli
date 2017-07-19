7f375be0cf1834fb5e6a15a244b5a343d30d9a98
ricofehr <ricofehr@nextdeploy.io>
2017-07-19 16:05:52 +0200

    Avoid too many pull on the docker image

--- docker.sh
+++ docker.sh
@@ -3,14 +3,26 @@
 # Generic script for launch the ndeploy docker image
 # @author Eric Fehr (ricofehr@nextdeploy.io, github: ricofehr)
 
+
+#################### CHANGE THIS 3 VALUES WITH YOUR NEXTDEPLOY AUTH INFOS
 USERNAME='usera@os.nextdeploy'
 PASSWORD='word123123'
 ENDPOINT='api.nextdeploy.local'
+####################
 
-mkdir -p ~/.nextdeploy
-echo "email: $USERNAME" > ~/.nextdeploy/nextdeploy.conf
-echo "password: $PASSWORD" >> ~/.nextdeploy/nextdeploy.conf
-echo "endpoint: $ENDPOINT" >> ~/.nextdeploy/nextdeploy.conf
+# Prepare the setting file
+if [[ ! -d ~/.nextdeploy ]]; then
+  mkdir -p ~/.nextdeploy
+  echo "email: $USERNAME" > ~/.nextdeploy/nextdeploy.conf
+  echo "password: $PASSWORD" >> ~/.nextdeploy/nextdeploy.conf
+  echo "endpoint: $ENDPOINT" >> ~/.nextdeploy/nextdeploy.conf
+fi
 
-docker pull nextdeploy/ndeploy >/dev/null 2>&1
+# Get last docker image
+if [[ ! -f /tmp/.ndeploy ]]; then
+  docker pull nextdeploy/ndeploy >/dev/null 2>&1
+  touch /tmp/.ndeploy
+fi
+
+# Launch the container
 docker run --rm -v ~/.ssh:/var/www/.ssh -v ~/.nextdeploy:/nextdeploy -v $PWD:/app -t -i nextdeploy/ndeploy $*
