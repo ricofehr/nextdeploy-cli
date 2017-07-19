99ed95f59b6ec676c6c08acc9b705e3b949f1b69
ricofehr <ricofehr@nextdeploy.io>
2017-07-19 15:43:38 +0200

    Silents docker pull from docker.sh

--- docker.sh
+++ docker.sh
@@ -12,5 +12,5 @@ echo "email: $USERNAME" > ~/.nextdeploy/nextdeploy.conf
 echo "password: $PASSWORD" >> ~/.nextdeploy/nextdeploy.conf
 echo "endpoint: $ENDPOINT" >> ~/.nextdeploy/nextdeploy.conf
 
-docker pull nextdeploy/ndeploy
+docker pull nextdeploy/ndeploy >/dev/null 2>&1
 docker run --rm -v ~/.ssh:/var/www/.ssh -v ~/.nextdeploy:/nextdeploy -v $PWD:/app -t -i nextdeploy/ndeploy $*
